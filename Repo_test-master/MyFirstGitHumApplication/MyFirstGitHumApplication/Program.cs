98072e976ddc03e0c981b34c46a8c3ccbab8d0c3
jenkins013 <31985262+jenkins013@users.noreply.github.com>
2017-10-17 20:41:45 +0530

    Octopack added

---
+++ Repo_test-master/MyFirstGitHumApplication/MyFirstGitHumApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHumApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!1234...trying to deploy");
+            Console.WriteLine("Change has been done");
+            Console.ReadLine();                
+        }
+    }
+}
